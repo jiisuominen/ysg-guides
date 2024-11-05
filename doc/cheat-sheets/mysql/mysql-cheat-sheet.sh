71804c7cfc348c4b50a8daf26930e53874c22535
Janne Suominen <janne.suominen@siili.com>
2024-11-05 08:41:58 +0200

    New sql command + AG search for printing.

--- doc/cheat-sheets/mysql/mysql-cheat-sheet.sh
+++ doc/cheat-sheets/mysql/mysql-cheat-sheet.sh
@@ -7,10 +7,22 @@ SELECT
 FROM
   information_schema.TABLES
 WHERE
-  TABLE_SCHEMA = "bookstore"
+  TABLE_SCHEMA = "avustusasiointitest"
 ORDER BY
   (DATA_LENGTH + INDEX_LENGTH)
 DESC;
 
-
+#Single table size
+SELECT
+  TABLE_NAME AS `Table`,
+  ROUND((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024) AS `Size (MB)`
+FROM
+  information_schema.TABLES
+WHERE
+    TABLE_SCHEMA = "bookstore"
+  AND
+    TABLE_NAME = "book"
+ORDER BY
+  (DATA_LENGTH + INDEX_LENGTH)
+DESC;
 
---
+++ doc/md/ag-search.md
@@ -0,0 +1,5 @@
+# Ag search
+
+sudo apt install silversearcher-ag
+
+https://github.com/ggreer/the_silver_searcher
